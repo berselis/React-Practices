7cee0174cfe675204936e7959dbbdd2bedddcb7a
Berselis Mendoza <berselismendoza@gmail.com>
2022-07-21 07:57:43 -0400

    Deploy
    update code

--- project-vite-bootstrap/src/App.jsx
+++ project-vite-bootstrap/src/App.jsx
@@ -1,6 +1,7 @@
 
 import reactLogo from './assets/react.svg';
 import bdevelopmentLogo from './assets/Media/imgs/logoWhite.png';
+import bootstrapLogo from './assets/bootstrap-logo-shadow.png';
 import './App.css';
 import PracticeOne from './Components/PracticeOne';
 import Morallogic from './Components/Morallogic';
@@ -13,6 +14,7 @@ function App() {
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
             <img src={reactLogo} alt="" className="d-inline-block align-text-top react-logo" />
+            <img src={bootstrapLogo} alt="" className="d-inline-block align-text-top bootstrap-logo" />
             <h3><strong>React Practices</strong> - BJMM</h3>
           </a>
           <img src={bdevelopmentLogo} alt="BJMM" className='bdevelopment-logo'/>
